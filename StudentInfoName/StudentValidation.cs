e6651dfbd9fa91cab3715285e283e4b936b6208c
VictorStankov <viktorstankov1@gmail.com>
2023-03-18 11:30:01 +0200

    feat: Add StudentValidation

---
+++ StudentInfoName/StudentValidation.cs
@@ -0,0 +1,13 @@
+﻿using UserLogin;
+using System.Linq;
+
+namespace StudentInfoName
+{
+    class StudentValidation
+    {
+        public Student GetStudentDataByUser(User user)
+        {
+            return (from student in StudentData.TestStudents where student.FacultyNum == user.FacultyNum select student).FirstOrDefault();
+        }
+    }
+}
