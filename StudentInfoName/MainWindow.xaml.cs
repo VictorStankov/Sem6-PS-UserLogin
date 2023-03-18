180fab9ae664f8ec0481d77819fd3b26203ad942
VictorStankov <viktorstankov1@gmail.com>
2023-03-19 00:28:18 +0200

    feat: Add helper methods and temp buttons

--- StudentInfoName/MainWindow.xaml.cs
+++ StudentInfoName/MainWindow.xaml.cs
@@ -24,5 +24,61 @@ namespace StudentInfoName
         {
             InitializeComponent();
         }
+
+        public void ClearAllInputs()
+        {
+            foreach (var field in MainGrid.Children)
+                if (field is TextBox)
+                    ((TextBox)field).Clear();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAllInputs();
+        }
+
+        public void LoadStudent(Student student)
+        {
+            FirstName.Text = student.FirstName;
+            MiddleName.Text = student.MiddleName;
+            LastName.Text = student.Surname;
+            Faculty.Text = student.Faculty;
+            Specialty.Text = student.Specialty;
+            Degree.Text = student.Degree;
+            Status.Text = student.Status.ToString();
+            FacultyNum.Text = student.FacultyNum.ToString();
+            Year.Text = student.Year.ToString();
+            Stream.Text = student.Stream.ToString();
+            Group.Text = student.Group.ToString();
+        }
+
+        private void LoadTestStudent_Click(object sender, RoutedEventArgs e)
+        {
+            LoadStudent(StudentData.TestStudents[0]);
+        }
+
+        public void SetInputsInactive()
+        {
+            foreach (var field in MainGrid.Children)
+                if (field is TextBox box)
+                    box.IsEnabled = false;
+        }
+
+        private void SetInactive_Click(object sender, RoutedEventArgs e)
+        {
+            SetInputsInactive();
+        }
+
+        public void SetInputsActive()
+        {
+            foreach (var field in MainGrid.Children)
+                if (field is TextBox box)
+                    box.IsEnabled = true;
+        }
+
+        private void SetActive_Click(object sender, RoutedEventArgs e)
+        {
+            SetInputsActive();
+        }
     }
 }
