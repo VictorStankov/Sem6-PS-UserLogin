2260186db37526237457fbd469b49dfd7dbe0f37
VictorStankov <viktorstankov1@gmail.com>
2023-03-18 11:21:53 +0200

    feat: Add student classes

---
+++ StudentInfoName/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentInfoName
+{
+    class Student
+    {
+        public string FirstName, MiddleName, Surname, Faculty, Specialty, Degree;
+        public int FacNum, Year, Stream, Group;
+
+        public Student(string firstName, string middleName, string surname, string faculty, string specialty,
+            string degree, int facNum, int year, int stream, int group)
+        {
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.Surname = surname;
+            this.Faculty = faculty;
+            this.Specialty = specialty;
+            this.Degree = degree;
+            this.FacNum = facNum;
+            this.Year = year;
+            this.Stream = stream;
+            this.Group = group;
+        }
+    }
+}
---
+++ StudentInfoName/StudentData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace StudentInfoName
+{
+    class StudentData
+    {
+        public List<Student> TestStudents
+        {
+            get
+            {
+                ResetTestStudents();
+                return _testStudents;
+            }
+            private set { }
+        }
+
+        private static List<Student> _testStudents;
+
+        public static void ResetTestStudents()
+        {
+            if (_testStudents == null)
+                _testStudents = new List<Student>();
+            else
+                _testStudents.Clear();
+            
+            _testStudents.Add(new Student("Ivan", "Ivanov", "Ivanov", "FCST", "CSE", "bachelor", 1, 1, 1, 1));
+        }
+    }
+}
