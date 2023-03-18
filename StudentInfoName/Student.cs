0d4d8c3b054cbf35dc2de78260144026a12bd968
VictorStankov <viktorstankov1@gmail.com>
2023-03-18 11:29:32 +0200

    fix: Rename attribute for consistency

--- StudentInfoName/Student.cs
+++ StudentInfoName/Student.cs
@@ -3,10 +3,10 @@
     class Student
     {
         public string FirstName, MiddleName, Surname, Faculty, Specialty, Degree;
-        public int FacNum, Year, Stream, Group;
+        public int FacultyNum, Year, Stream, Group;
 
         public Student(string firstName, string middleName, string surname, string faculty, string specialty,
-            string degree, int facNum, int year, int stream, int group)
+            string degree, int facultyNum, int year, int stream, int group)
         {
             this.FirstName = firstName;
             this.MiddleName = middleName;
@@ -14,7 +14,7 @@
             this.Faculty = faculty;
             this.Specialty = specialty;
             this.Degree = degree;
-            this.FacNum = facNum;
+            this.FacultyNum = facultyNum;
             this.Year = year;
             this.Stream = stream;
             this.Group = group;
