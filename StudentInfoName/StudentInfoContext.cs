776ff9be44cff008412ce00251b8f2fcb53404fb
VictorStankov <viktorstankov1@gmail.com>
2023-03-26 20:07:23 +0300

    feat: Add context classes

---
+++ StudentInfoName/StudentInfoContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Configuration;
+using UserLogin;
+
+namespace StudentInfoSystem
+{
+    class StudentInfoContext : DbContext
+    {
+        public DbSet<Student> Studets { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public StudentInfoContext() : base(ConfigurationManager.ConnectionStrings["DbConnection"].ToString())
+        {
+            
+        }
+    }
+}
---
+++ UserLogin/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Configuration;
+
+namespace UserLogin
+{
+    class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserContext() : base(ConfigurationManager.ConnectionStrings["DbConnection"].ToString())
+        {
+
+        }
+    }
+}
