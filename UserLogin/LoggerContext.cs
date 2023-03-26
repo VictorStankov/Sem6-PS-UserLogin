1ec14bdcb8be44f0a7af8fce16bc815a0f7aacf7
VictorStankov <viktorstankov1@gmail.com>
2023-03-26 23:48:30 +0300

    feat: Add logging to DB

---
+++ UserLogin/LoggerContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Configuration;
+
+namespace UserLogin
+{
+    class LoggerContext : DbContext
+    {
+        public DbSet<LogEvent> Logs { get; set; }
+
+        public LoggerContext() : base(ConfigurationManager.ConnectionStrings["DbConnection"].ToString())
+        {
+
+        }
+    }
+}
