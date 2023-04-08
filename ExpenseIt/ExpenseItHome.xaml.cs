9ec5b15457aa9f292f55b77c774d3bec7ae68679
VictorStankov <viktorstankov1@gmail.com>
2023-04-08 19:20:37 +0300

    feat: Add some data bindings

--- ExpenseIt/ExpenseItHome.xaml.cs
+++ ExpenseIt/ExpenseItHome.xaml.cs
@@ -19,6 +19,8 @@ namespace ExpenseIt
     /// </summary>
     public partial class ExpenseItHome : Window
     {
+        public string MainCaptionText { get; set; }
+
         public ExpenseItHome()
         {
             InitializeComponent();
